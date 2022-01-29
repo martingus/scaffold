dd0d92f800d15b4819f9e78c9b2050b99a82e04a
Martin Gustafsson <mistermartin.gustafsson@gmail.com>
2022-01-29 22:43:58 +0000

    updated files as per course instructions

--- hello.py
+++ hello.py
@@ -1,6 +1,17 @@
-def add(x, y):
-    return x + y
+def toyou(x):
+    return f"hi {x}"
+
+def add(x):
+    return x + 1
+
+def subtract(x):
+    return x - 1
+    
+
+
+#def add(x, y):
+#    return x + y
 
 #var=
-result = add(1, 2)
-print(f"This is the sum of 1 and 2: {result}")
+#result = add(1, 2)
+#print(f"This is the sum of 1 and 2: {result}")
